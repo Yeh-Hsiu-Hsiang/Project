781ee1d9cde350c2bea585a9f5d1de32f2480acb
eileenlo <N1056407@ntub.edu.tw>
2019-05-01 02:21:16 +0800

    add 6 buttons

--- clothes/app/src/main/java/com/example/clothes/MainActivity.java
+++ clothes/app/src/main/java/com/example/clothes/MainActivity.java
@@ -23,32 +23,70 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1 = (Button) findViewById(R.id.button1);
         Button button2 = (Button) findViewById(R.id.button2);
-        //動畫路徑設定(x1,x2,y1,y2)
-        TranslateAnimation am = new TranslateAnimation(0, -500, 0, -700);
-        am.setDuration(1000);
-        //am.setRepeatMode(am.REVERSE);
-        button1.setAnimation(am);
-        //am.setFillAfter(true);
-        button1.startAnimation(am);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
 
-        TranslateAnimation am2 = new TranslateAnimation(0, -200, 0, -700);
+        TranslateAnimation am1 = new TranslateAnimation(0, -450, 0, -500);
+        am1.setDuration(1000);
+        button1.setAnimation(am1);
+        button1.startAnimation(am1);
+
+        TranslateAnimation am2 = new TranslateAnimation(0, -270, 0, -600);
         am2.setDuration(1000);
         button2.setAnimation(am2);
         button2.startAnimation(am2);
+
+        TranslateAnimation am3 = new TranslateAnimation(0, -90, 0, -700);
+        am3.setDuration(1000);
+        button3.setAnimation(am3);
+        button3.startAnimation(am3);
+
+        TranslateAnimation am4 = new TranslateAnimation(0, 90, 0, -700);
+        am4.setDuration(1000);
+        button4.setAnimation(am4);
+        button4.startAnimation(am4);
+
+        TranslateAnimation am5 = new TranslateAnimation(0, 270, 0, -600);
+        am5.setDuration(1000);
+        button5.setAnimation(am5);
+        button5.startAnimation(am5);
+
+        TranslateAnimation am6 = new TranslateAnimation(0, 450, 0, -500);
+        am6.setDuration(1000);
+        button6.setAnimation(am6);
+        button6.startAnimation(am6);
 
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable(){
             Button button1 = (Button) findViewById(R.id.button1);
             Button button2 = (Button) findViewById(R.id.button2);
+            Button button3 = (Button) findViewById(R.id.button3);
+            Button button4 = (Button) findViewById(R.id.button4);
+            Button button5 = (Button) findViewById(R.id.button5);
+            Button button6 = (Button) findViewById(R.id.button6);
             @Override
             public void run() {
                 //過兩秒後要做的事情
-                button1.setTranslationX(-500);
-                button1.setTranslationY(-700);
+                button1.setTranslationX(-450);
+                button1.setTranslationY(-500);
 
-                button2.setTranslationX(-200);
-                button2.setTranslationY(-700);
+                button2.setTranslationX(-270);
+                button2.setTranslationY(-600);
+
+                button3.setTranslationX(-90);
+                button3.setTranslationY(-700);
+
+                button4.setTranslationX(90);
+                button4.setTranslationY(-700);
+
+                button5.setTranslationX(270);
+                button5.setTranslationY(-600);
+
+                button6.setTranslationX(450);
+                button6.setTranslationY(-500);
                 //Toast.makeText(MainActivity.this, "Hi", Toast.LENGTH_SHORT).show();
             }}, 999);
 
