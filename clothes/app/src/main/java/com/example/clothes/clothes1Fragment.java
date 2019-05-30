308418357504286969cfb872503bb2cc1b9df39e
eileenlo <N1056407@ntub.edu.tw>
2019-05-30 14:46:04 +0800

    Integration of code 2

--- clothes/app/src/main/java/com/example/clothes/clothes1Fragment.java
+++ clothes/app/src/main/java/com/example/clothes/clothes1Fragment.java
@@ -1,29 +1,20 @@
 package com.example.clothes;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class clothes1Fragment extends Fragment {
@@ -32,6 +23,7 @@ public class clothes1Fragment extends Fragment {
     static final String tb_name = "ManageClothes";
     static int count = 0;
     SQLiteDatabase db;
+
     private View view;//定義view用來設置fragment的layout
     public RecyclerView mCollectRecyclerView;//定義RecyclerView
     //定義getClothesMember的集合
@@ -50,7 +42,6 @@ public class clothes1Fragment extends Fragment {
 //        initRecyclerView();
 //        //建立數據
 
-
         return view;
     }
 
@@ -58,7 +49,7 @@ public class clothes1Fragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         if (clothesList != null)
-            clothesList=null;
+            clothesList = null;
 
         clothesList = new ArrayList<>();
         initData();
@@ -108,7 +99,6 @@ public class clothes1Fragment extends Fragment {
         db.close();
         Log.e("count","fragmant"+Integer.toString(count));
         count = 0;
-        //點別的fragment再回來會重新再載入??
     }
     private void initRecyclerView(){
         mCollectRecyclerView = (RecyclerView)view.findViewById(R.id.collect_recyclerView);
@@ -119,11 +109,13 @@ public class clothes1Fragment extends Fragment {
         mCollectRecyclerView.addItemDecoration(new MyPaddingDecoration());
 
         //點擊進入修改
-        memberAdapter.setOnItemClickListener(new MemberAdapter.OnItemClickListener() {
+        memberAdapter.setOnItemLongClickListener(new MemberAdapter.OnItemLongClickListener() {
             @Override
-            public void OnItemClick(View view ,getClothesMember data) {
+            public void OnItemLongClick(View view ,getClothesMember data) {
                 //此處進行監聽事件的動作處理
                 Toast.makeText(getActivity(),data.id,Toast.LENGTH_SHORT).show();
+
+                //next----
             }
         });
 
