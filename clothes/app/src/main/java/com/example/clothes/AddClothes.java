4f735fe46bd592ab7749df0d87882d58ee3432b2
Yeh <apple22872980@yahoo.com.tw>
2019-10-07 14:22:41 +0800

    Delete unnecessary files

--- clothes/app/src/main/java/com/example/clothes/AddClothes.java
+++ clothes/app/src/main/java/com/example/clothes/AddClothes.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.gabrielbb.cutout.CutOut;
 
