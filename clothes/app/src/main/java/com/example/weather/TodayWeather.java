2a7bbc37f073dc8e933c5e60b061c86419ddcfe2
Yeh <apple22872980@yahoo.com.tw>
2019-10-12 21:33:37 +0800

    change welcome and weather screen

--- clothes/app/src/main/java/com/example/weather/TodayWeather.java
+++ clothes/app/src/main/java/com/example/weather/TodayWeather.java
@@ -28,8 +28,7 @@ public class TodayWeather {
             StringBuilder sb = new StringBuilder ();
             try {
                 URL url = new URL ( params[0] );
-                BufferedReader in = new BufferedReader (
-                        new InputStreamReader ( url.openStream () ) );
+                BufferedReader in = new BufferedReader (new InputStreamReader ( url.openStream () ) );
                 String line = in.readLine ();
                 while (line != null) {
                     Log.d ( "HTTP", line );
@@ -44,33 +43,33 @@ public class TodayWeather {
             return sb.toString ();
         }
 
-        protected void onPostExecute(String s) {
-            super.onPostExecute ( s );
-            Log.d ( "JSON", s );
-            parseJSON ( s );
+        protected void onPostExecute(String data) {
+            super.onPostExecute ( data );
+            Log.d ( "JSON", data );
+            parseJSON ( data );
         }
 
-        private void parseJSON(String s) {
-        }
-    }
+        private void parseJSON(String data)  {
+            Log.d("data","data = " + data);
+            JSONObject Ob;
+            try{
+                Ob = new JSONObject(data);
+                Object jsonOb = Ob.getJSONObject("cwbopendata").get("dataset");
+                Log.d("dataset","Object = " + jsonOb);
 
-    public void main(String[] args) {
-        try {
-            //建立一個JSONArray並帶入JSON格式文字，getString(String key)取出欄位的數值
-            String jsonText = null;
-            JSONArray JsonArray  = new JSONArray ( jsonText );
-            // Get all jsonObject from jsonArray
-            for (int i = 0; i < JsonArray .length (); i++) {
-                JSONObject JsonObject = JsonArray .getJSONObject ( i );
-                // 縣市
-                String locationName = JsonObject.getString ( "locationName" );
-                // 天氣因子:Wx, PoP, CI, MinT, MaxT
-                String elementName = JsonObject.getString ( "elementName" );
-                // 降雨機率
-                String parameter = JsonObject.getString ( "parameter" );
+                JSONArray array = Ob.getJSONObject("cwbopendata").getJSONObject("dataset").getJSONArray ("location");
+                Log.d("array", "array = " + array);
+
+                for (int i = 0; i < array .length (); i++) {
+                    JSONObject JsonObject = array.getJSONObject(i);
+                    Log.d("jsonObject", "json = " + JsonObject);
+                    String locationName = JsonObject.getString("locationName");
+                    Log.d("TAG", "城市：:" + locationName);
+                }
             }
-        } catch (JSONException e) {
-            e.printStackTrace ();
+            catch(JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
