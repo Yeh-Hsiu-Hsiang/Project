3c69e3379684908f3708496a2de90163b1e3b459
Yeh <apple22872980@yahoo.com.tw>
2019-10-17 22:03:48 +0800

    get select date and add switch in event

--- clothes/app/src/main/java/com/example/calendar/AddSchedule.java
+++ clothes/app/src/main/java/com/example/calendar/AddSchedule.java
@@ -4,18 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
-import com.example.clothes.MainActivity;
 import com.example.clothes.R;
-
 
 
 public class AddSchedule extends AppCompatActivity {
 
+    private Switch swh_status;
+    boolean is_enable = true;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+        swh_status = (Switch) findViewById(R.id.swh_status);
+        swh_status.setChecked(false);
+        swh_status.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (buttonView.isChecked()) {
+                    Log.d("swh = ", "On");
+                   //顯示所選日期
+                } else {
+                    Log.d("swh = ", "Off");
+                    //顯示所選日期範圍
+                }
+            }
+        });
     }
 
     // 取消按鈕
@@ -26,7 +45,7 @@ public class AddSchedule extends AppCompatActivity {
         AddSchedule.this.finish();
     }
 
-    // 取消按鈕
+    // 確認按鈕
     public void stockpile(View view) {
         Intent intent = new Intent();
         intent.setClass(AddSchedule.this, Calendar.class);
--- clothes/app/src/main/java/com/example/calendar/Calendar.java
+++ clothes/app/src/main/java/com/example/calendar/Calendar.java
@@ -1,14 +1,17 @@
 package com.example.calendar;
 
+import android.app.MediaRouteButton;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.example.clothes.MainActivity;
 import com.example.clothes.R;
+
 
 public class Calendar extends AppCompatActivity {
 
@@ -16,7 +19,19 @@ public class Calendar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        CalendarView calendarView = (CalendarView) findViewById(R.id.calendar);
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            // month 預設 0-11
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int dayOfMonth) {
+                Toast.makeText(Calendar.this,"您選的日期是" + year + "年" + (month+1)  + "月" + dayOfMonth + "日", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    // 回到今天
+    public void today(View view) {
+
     }
 
     // 新增活動按鈕
