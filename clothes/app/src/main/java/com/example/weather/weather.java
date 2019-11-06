6f46b6194c022a0cc60c83ec0c571a4434e0d5fa
Yeh <apple22872980@yahoo.com.tw>
2019-11-06 18:42:55 +0800

    Change icon

--- clothes/app/src/main/java/com/example/weather/weather.java
+++ clothes/app/src/main/java/com/example/weather/weather.java
@@ -1,19 +1,8 @@
 package com.example.weather;
 
-import android.Manifest;
-import android.app.assist.AssistStructure;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
